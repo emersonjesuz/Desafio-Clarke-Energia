8da1bf8f1e7e4855334d6e01fe0a0efeae35b296
emersonjesuz <josiemerson2013@gmail.com>
2024-10-20 13:45:13 -0300

    fix(server): Add repository in provider module

--- server/src/graphql/Company/company.module.ts
+++ server/src/graphql/Company/company.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { CompanyService } from './company.service';
 import { CompanyResolver } from './company.resolver';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { CompanyRepository } from './company.repository';
 
 @Module({
-  providers: [CompanyService, CompanyResolver, PrismaService],
+  providers: [
+    CompanyService,
+    CompanyResolver,
+    PrismaService,
+    CompanyRepository,
+  ],
   exports: [CompanyService],
 })
 export class CompanyModule {}
