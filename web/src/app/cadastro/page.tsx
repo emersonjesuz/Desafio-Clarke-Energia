6eb33cddca6d0c6a38b7d7413925d24d0e9cc46c
emersonjesuz <josiemerson2013@gmail.com>
2024-10-21 14:06:43 -0300

    feat(web): create screen cadastro

---
+++ web/src/app/cadastro/page.tsx
@@ -0,0 +1,18 @@
+import FormCadastro from "@/components/formCadastro";
+import LogoClarkeGreen from "@/components/icons/logoClarkeGreen";
+
+export default function Cadastro() {
+  return (
+    <div className="flex min-h-screen w-screen items-center justify-center bg-[#f2f2f2] p-4 py-32 lg:h-[80%]">
+      <div className="flex w-full max-w-[500px] flex-col items-center justify-center gap-10 rounded-xl bg-white p-4 lg:p-8">
+        <section className="flex w-full items-center justify-between bg-white">
+          <LogoClarkeGreen className="h-[63px] w-[116px]" />
+          <h1 className="text-md text-center font-poppins font-bold text-greenClarke lg:text-2xl">
+            Seja bem-vindo
+          </h1>
+        </section>
+        <FormCadastro />
+      </div>
+    </div>
+  );
+}
