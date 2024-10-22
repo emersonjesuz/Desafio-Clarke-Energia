a2f5c92a73c72618e874672dec79ca185ff280b7
emersonjesuz <josiemerson2013@gmail.com>
2024-10-22 13:52:37 -0300

    feat(server): Add integration of the login and registration page with API

--- web/src/components/formCadastro/index.tsx
+++ web/src/components/formCadastro/index.tsx
@@ -9,9 +9,34 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
+import { gql, useMutation } from "@apollo/client";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
+
+const CREATE_COMPANY = gql`
+  mutation PostCreateCompany(
+    $email: String!
+    $cnpj: String!
+    $kwh: Float!
+    $name: String!
+    $phone: String!
+  ) {
+    createCompany(
+      company: {
+        email: $email
+        cnpj: $cnpj
+        kwh: $kwh
+        name: $name
+        phone: $phone
+      }
+    ) {
+      id
+    }
+  }
+`;
 
 const formSchema = z.object({
   email: z.string().email({ message: "Email preciso ser válido" }),
@@ -21,30 +46,75 @@ const formSchema = z.object({
   phone: z
     .string()
     .length(11, { message: "Telefone preciso ter 11 caracteres" }),
-  password: z.string(),
 });
 export default function FormCadastro() {
+  const router = useRouter();
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
       cnpj: "",
       kwh: "",
-      password: "",
       name: "",
       phone: "",
     },
   });
+
+  const [createCompany, { error, data }] = useMutation(CREATE_COMPANY);
 
   function extractDigits(input: string): string {
     return input.replace(/\D/g, "");
   }
 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+    createCompany({
+      variables: {
+        email: values.email,
+        cnpj: values.cnpj,
+        kwh: +values.kwh,
+        name: values.name,
+        phone: values.phone,
+      },
+    }).catch(() => {
+      // O foi tratado na função handlerMessageError
+    });
   }
+
+  function handlerMessageError() {
+    switch (error?.message.split(" ")[0]) {
+      case "Nome":
+        form.setError("name", { message: error.message });
+        break;
+      case "Email":
+        form.setError("email", { message: error.message });
+        break;
+
+      case "CNPJ":
+        form.setError("cnpj", { message: error.message });
+        break;
+      case "KWH":
+        form.setError("kwh", { message: error.message });
+        break;
+      case "Telefone":
+        form.setError("phone", { message: error.message });
+        break;
+
+      default:
+        break;
+    }
+  }
+
+  useEffect(() => {
+    if (data?.createCompany) {
+      router.push(`/fornecedores/${data.createCompany.id}`);
+    }
+  }, [data]);
+
+  useEffect(() => {
+    if (error?.message) {
+      handlerMessageError();
+    }
+  }, [error]);
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-8">
@@ -126,6 +196,7 @@ export default function FormCadastro() {
                 <Input
                   type="tel"
                   required
+                  maxLength={11}
                   className="h-12 w-full"
                   placeholder=""
                   onChange={(e) => {
