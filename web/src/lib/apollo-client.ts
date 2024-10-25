4684be624837e30432666934c7b34d7331830d03
emersonjesuz <josiemerson2013@gmail.com>
2024-10-24 23:54:12 -0300

    fix(web): toggle url to production

--- web/src/lib/apollo-client.ts
+++ web/src/lib/apollo-client.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: "http://localhost:3000/graphql", //"https://desafio-clarke-energia-production.up.railway.app/graphql",
+  uri: "https://desafio-clarke-energia-production.up.railway.app/graphql",
   cache: new InMemoryCache(),
 });
 
